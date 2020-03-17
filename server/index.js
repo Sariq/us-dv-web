1f2f95d0735536ab003301458c4e1f0b921501a7
System Administrator <root@Saris-MacBook-Pro.local>
2020-03-17 21:22:04 +0200

    heroku build packagejson

--- server/index.js
+++ server/index.js
@@ -30,12 +30,15 @@ if(!isProduction) {
   mongoose.connect('mongodb://localhost/us-dv');
 
 }else{
-  app.use(express.static('../client/build'));
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
   mongoose.connect('mongodb+srv://developer:London2020@cluster0-8wbi3.mongodb.net/', {dbName: 'maldives-converter'});
 }
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, '../client/build')))
+
+// app.get('*', function(request, response) {
+//   response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+// });
+
 //Configure Mongoose
 //mongoose.connect('mongodb+srv://developer:London2020@cluster0-8wbi3.mongodb.net/', {dbName: 'maldives-converter'});
 
@@ -50,7 +53,9 @@ require('./models/Users');
 require('./models/Converters');
 require('./config/passport');
 app.use(require('./routes'));
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+})
 //Error handlers & middlewares
 if(!isProduction) {
   app.use((err, req, res, next) => {
