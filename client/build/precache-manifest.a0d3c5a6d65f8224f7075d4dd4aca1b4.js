2c7966b8ef9c2614f12ec2cfb89971d56214dad1
System Administrator <root@Saris-MacBook-Pro.local>
2020-03-17 21:33:00 +0200

    heroku build packagejson

---
+++ client/build/precache-manifest.a0d3c5a6d65f8224f7075d4dd4aca1b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3495f0b6faa1e87b2f29d98e9475afee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83f48d31d214daa7a541",
+    "url": "/static/css/main.9c54194a.chunk.css"
+  },
+  {
+    "revision": "377a2db2a0eff6faaee8",
+    "url": "/static/js/2.e25c9937.chunk.js"
+  },
+  {
+    "revision": "78964249a57897faa69c0cbc39ad4ae3",
+    "url": "/static/js/2.e25c9937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83f48d31d214daa7a541",
+    "url": "/static/js/main.63db8e10.chunk.js"
+  },
+  {
+    "revision": "c6f18f983160b31f0505",
+    "url": "/static/js/runtime-main.6823e00b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
