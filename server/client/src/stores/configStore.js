7efd8c9254c9c13dd69d5fa17ebbd2cf178bf308
System Administrator <root@Saris-MacBook-Pro.local>
2020-03-18 02:03:59 +0200

    heroku build packagejson

--- server/client/src/stores/configStore.js
+++
@@ -1,8 +0,0 @@
-import { observable} from "mobx";
-console.log(window.location.hostname) 
-class ConfigStore {
-    @observable configData={
-        domain : window.location.hostname === "localhost" ? 'http://localhost:8000/' : "https://us-dv.herokuapp.com/" 
-    };
-}
-export default new ConfigStore();
--- server/client/src/stores/index.js
+++
@@ -1,9 +0,0 @@
-import registrationStore from "./registrationStore"
-import ConfigStore from "./configStore"
-import AuthStore from "./AuthStore"
-
-export default {
-    registrationStore,
-    ConfigStore,
-    AuthStore
-}
